91bdd7677a05c7d6ee783c2a104ad4e971877679
quaintpelican <cwitzaney@gmail.com>
2026-08-22 17:53:07 -0700

    Implement Audist V3.2 system coverage

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="audist-v3-1-reliability-1";
+const CACHE="audist-v3-2-system-coverage-1";
 const ASSETS=["./","./index.html","./styles.css","./db.js","./app.js","./manifest.webmanifest","./icon.svg","./audist-icon.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
